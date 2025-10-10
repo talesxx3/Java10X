dd25935d9e90dd8012ed4689ef49616f2964474f
devtales <tales_silva2013@live.com>
2025-10-09 22:09:47 -0300

    Stack

---
+++ src/NivelIntermediario/ArrayListStack/Main.java
@@ -0,0 +1,44 @@
+package NivelIntermediario.ArrayListStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Arrays
+        System.out.println("----------- ARRAY ------------");
+
+        String[] nomeNinjaArray = new String[7];
+        nomeNinjaArray[0] = "Naruto Uzumaki";
+        nomeNinjaArray[1] = "Sasuke Uchiha";
+        nomeNinjaArray[2] = "Sakura Haruno";
+        nomeNinjaArray[3] = "Hinata Hyuga";
+        System.out.println("nomeNinjaArray = " + nomeNinjaArray);
+        System.out.println("nomeNinjaArray[0] = " + nomeNinjaArray[4]);
+
+        // Lista é um pouco mais lenta em comparacao com ARRAY
+        System.out.println("----------- LISTA ------------");
+        List<String> nomeNinjaList = new ArrayList<>();
+        nomeNinjaList.add("Naruto Uzumaki");
+        nomeNinjaList.add("Sasuke Uchiha");
+        nomeNinjaList.add("Sakura Haruno");
+        nomeNinjaList.add("Madara Uchiha");
+        System.out.println(nomeNinjaList);
+
+        System.out.println("----------- STACK ------------");
+        Stack<String> nomeNinjaStack = new Stack<>();
+        nomeNinjaStack.push("Naruto Uzumaki");
+        nomeNinjaStack.push("Sasuke Uchiha");
+        nomeNinjaStack.push("Sakura Haruno");
+        System.out.println("Mostrar Stack = " + nomeNinjaStack);
+        System.out.println("Tirar da pilha/stack = " + nomeNinjaStack.pop());
+        System.out.println("Mostrar Stack = " + nomeNinjaStack);
+        System.out.println("Tirar da pilha/stack = " + nomeNinjaStack.pop());
+        nomeNinjaStack.push("Kakashi");
+        System.out.println("Mostrar Stack = " + nomeNinjaStack);
+        System.out.println("Ninja no topo (sem remover): " + nomeNinjaStack.peek());
+        System.out.println("Mostrar Stack = " + nomeNinjaStack);
+    }
+}
